0e603342686649a9057c8583f983198f661e9201
Sebastian Bielfeldt <50203100+cvazz@users.noreply.github.com>
2026-04-17 13:03:20 +0200

    updating dataset configs

--- examples/notebooks/dataset_configs.py
+++ examples/notebooks/dataset_configs.py
@@ -72,6 +72,8 @@ def apply_config_myoglobin_general(idx: str | int) -> dict:
         ),
     )
     return config.model_dump()
+
+
 # --- Helper for rsEGFP2 ---
 
 
@@ -103,15 +105,16 @@ def apply_config_rsEGFP2() -> dict:
     return config.model_dump()
 
 
-
 # --- Helper for Photolyase ---
 
 
-def apply_config_PL_general(name_ending: str | int, add_light=False) -> dict:
+def apply_config_PL_general(
+    name_ending: str | int, add_light=False, diff=False
+) -> dict:
     homepath = load_homepath()
     folderloc = f"{homepath}examples/data/photolyase/"
 
-    folders = [f for f in os.listdir(folderloc) if f[:2] != "1_"]
+    folders = [f for f in os.listdir(folderloc) if f[:2] != "1_" or f[:1] != "."]
     out = None
     for ii, f in enumerate(folders):
         if (isinstance(name_ending, str) and f.endswith(name_ending)) or (
@@ -125,42 +128,45 @@ def apply_config_PL_general(name_ending: str | int, add_light=False) -> dict:
 
     final = out.split("_")[-1]
     changing_bit = f"{out}/{final}"
-
+    gen_settings = GeneralSettings(
+        name_machine=f"PL_{final}",
+        name_human=f"Photolyase {final}",
+        high_resolution_limit=2.6,
+    )
+    input_files = InputFileSettings(
+        map_dark=f"{folderloc}1_superdark/superdark_deposit.mtz",
+        pdb_dark=f"{folderloc}1_superdark/superdark_deposit.pdb",
+        pdb_triggered=f"{folderloc}{changing_bit}_deposit.pdb" if add_light else None,
+        columns_dark=ColumnConfig(
+            amplitude_column="F-obs-filtered",
+            uncertainty_column="SIGF-obs-filtered",
+            phase_column="PHIF-model",
+        ),
+    )
+    if diff:
+        input_files.map_diff = f"{folderloc}{changing_bit}-dark_kwt_ded.mtz"
+        input_files.columns_diff = ColumnConfig(
+            amplitude_column="KFOFOWT",
+            uncertainty_column="SIGF",
+            phase_column="PHIKFOFOWT",
+        )
+        gen_settings.comparison_type = "diff"
+    else:
+        input_files.map_triggered = f"{folderloc}{changing_bit}_deposit.mtz"
+        input_files.columns_triggered = ColumnConfig(
+            amplitude_column="F",
+            uncertainty_column="SIGF",
+            phase_column="PHIF-model",
+        )
     config = Settings(
-        general=GeneralSettings(
-            name_machine=f"PL_{final}",
-            name_human=f"Photolyase {final}",
-            high_resolution_limit=2.6,
-        ),
-        input_files=InputFileSettings(
-            map_dark=f"{folderloc}1_superdark/superdark_deposit.mtz",
-            map_triggered=f"{folderloc}{changing_bit}_deposit.mtz",
-            pdb_dark=f"{folderloc}1_superdark/superdark_deposit.pdb",
-            pdb_triggered=(
-                f"{folderloc}{changing_bit}_deposit.pdb" if add_light else None
-            ),
-            columns_dark=ColumnConfig(
-                amplitude_column="F-obs-filtered",
-                uncertainty_column="SIGF-obs-filtered",
-                phase_column="PHIF-model",
-            ),
-            columns_triggered=ColumnConfig(
-                amplitude_column="F",
-                uncertainty_column="SIGF",
-                phase_column="PHIF-model",
-            ),
-        ),
+        general=gen_settings,
+        input_files=input_files,
     )
     return config.model_dump()
 
 
 # --- Helper for B12 (Difference Map Path) ---
-
-
-def apply_config_B12_general(idx: int) -> dict:
-    homepath = load_homepath()
-    folderloc = f"{homepath}data/b12_sacla/"
-
+def get_b12_diffmap_paths(folderloc: str, idx: int | str):
     diffmap_locs = sorted([f for f in os.listdir(folderloc) if "qFoFo" in f])
     # Reproduce your delete logic: np.delete(diffmap_locs, [1])
     diffmap_locs = [f for i, f in enumerate(diffmap_locs) if i != 1]
@@ -175,29 +181,68 @@ def apply_config_B12_general(idx: int) -> dict:
         "3us_12mJ.cm-2_SACLA_qFoFo.mtz": "9S0D",
         "3us_30mJ.cm-2_SACLA_qFoFo.mtz": "9S0E",
     }.get(diffmap_name)
+    return diffmap_name, b12_pdb
 
+
+def get_folders_B12_light_with_pdb(folderloc: str, idx: int | str):
+    lightmap_locs = os.listdir(folderloc)
+    lightmap_locs = [f for f in lightmap_locs if "light_FPFree" in f]
+    lightmap_locs = np.sort(lightmap_locs)
+    lightmap_locs = np.delete(lightmap_locs, [1])
+    lightmap_name = str(lightmap_locs[idx])
+    logger.info(f"Selected lightmap: {lightmap_name}")
+    b12_pdb = {
+        "100us_30mJ.cm-2_light_FPFree.mtz": "9S0B",
+        "10ns_12mJ.cm-2_light_FPFree.mtz": "9S0C",
+        "10ns_30mJ.cm-2_light_FPFree.mtz": "9S08",
+        "300ns_30mJ.cm-2_light_FPFree.mtz": "9S09",
+        "3ms_30mJ.cm-2_light_FPFree.mtz": "9S0B",
+        "3us_120mJ.cm-2_light_FPFree.mtz": None,
+        "3us_12mJ.cm-2_light_FPFree.mtz": "9S0D",
+        "3us_30mJ.cm-2_light_FPFree.mtz": "9S0E",
+        "3us_60mJ.cm-2_light_FPFree.mtz": None,
+    }.get(lightmap_name)
+    return lightmap_name, b12_pdb
+
+
+def apply_config_B12_general(idx: int, diff=False) -> dict:
+    homepath = load_homepath()
+    folderloc = f"{homepath}examples/data/b12_sacla/"
+
+    if diff:
+        othermap_name, b12_pdb = get_b12_diffmap_paths(folderloc, idx)
+    else:
+        othermap_name, b12_pdb = get_folders_B12_light_with_pdb(folderloc, idx)
+
+    general = GeneralSettings(
+        name_machine=f"B12_{othermap_name.split('mJ')[0]}",
+        high_resolution_limit=2.3,
+    )
+    input_files = InputFileSettings(
+        map_dark=f"{folderloc}9S06_dark.mtz",
+        pdb_dark=f"{folderloc}9S06.pdb",
+        pdb_triggered=f"{folderloc}{b12_pdb}" if b12_pdb else None,
+        columns_dark=ColumnConfig(
+            amplitude_column="FP", phase_column="MODEL", uncertainty_column="SIGFP"
+        ),
+    )
+    if diff:
+        general.comparison_type = "diff"
+        input_files.map_diff = f"{folderloc}{othermap_name}"
+        input_files.columns_diff = DiffColumnConfig(
+            amplitude_column="QFOFOWT",
+            uncertainty_column="SIGF",
+            phase_column="PHIQFOFOWT",
+        )
+    else:
+        general.comparison_type = "triggered"
+        input_files.map_triggered = f"{folderloc}{othermap_name}"
+        input_files.columns_triggered = ColumnConfig(
+            amplitude_column="F", phase_column="MODEL", uncertainty_column="SIGF"
+        )
     config = Settings(
-        general=GeneralSettings(
-            name_machine=f"B12_{diffmap_name.split('mJ')[0]}",
-            high_resolution_limit=2.3,
-            comparison_type="diff",
-        ),
-        input_files=InputFileSettings(
-            map_dark=f"{folderloc}9S06_dark.mtz",
-            map_diff=f"{folderloc}{diffmap_name}",
-            pdb_dark=f"{folderloc}9S06.pdb",
-            pdb_triggered=f"{folderloc}{b12_pdb}.pdb" if b12_pdb else None,
-            columns_dark=ColumnConfig(
-                amplitude_column="FP",
-                phase_column="PHIF-model",
-                uncertainty_column="SIGFP",
-            ),
-            columns_diff=DiffColumnConfig(
-                amplitude_column="QFOFOWT",
-                uncertainty_column="SIGF",
-                phase_column="PHIQFOFOWT",
-            ),
-        ),
+        general=general,
+        input_files=input_files,
     )
     return config.model_dump()
 
@@ -211,7 +256,7 @@ def main():
 
 
 if __name__ == "__main__":
-        # print(get_folders_B12_diff_with_pdb())
+    # print(get_folders_B12_diff_with_pdb())
     # print(get_folders_B12())
     # print(apply_config_B12_general_light(0))
     main()
