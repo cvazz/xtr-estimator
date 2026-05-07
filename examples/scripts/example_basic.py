a6e8b71ccc07916662c04b6470ff8fc31dff86fd
Sebastian Bielfeldt <50203100+cvazz@users.noreply.github.com>
2026-05-07 10:58:30 +0200

    improving example scripts

--- examples/scripts/example_basic.py
+++ examples/scripts/example_basic.py
@@ -1,4 +1,5 @@
 from xtr_estimator.configuration import load_homepath
+from xtr_estimator.estimation import plot_extrapolation_estimate
 from xtr_estimator.main import execute_as_main
 
 
@@ -11,6 +12,7 @@ from xtr_estimator.configuration import (
     PlotSettings,
     MaskingSettings
 )
+from xtr_estimator.processing import get_maps, prepare_maps
 
 
 def apply_config_rsEGFP2() -> dict:
@@ -54,19 +56,41 @@ def apply_config_rsEGFP2() -> dict:
 
 
     # Return as a dictionary for backwards compatibility with your existing pipeline
-    return config.model_dump()
+    return config
 
-def main():
+from xtr_estimator.masking import make_inclusion_mask
+from xtr_estimator.xtr_maps import save_to_folder
+
+def simple():
     # Load defaults + local yaml
     cfg = apply_config_rsEGFP2()
-
-    
     # Manual override in code
     # cfg.general.name_human = "Modified_Experiment (Example)"
     
-    execute_as_main(cfg, save2file=True)
+    prediction_tuple = execute_as_main(cfg, save2file=True)
+    return prediction_tuple
+
+def extrapolation():
+    config = apply_config_rsEGFP2()
+    unscaled_dark, unscaled_triggered = get_maps(config)
+    diffmap, map_dark, _ = prepare_maps(unscaled_dark, unscaled_triggered, config)
+    inclusion_mask = make_inclusion_mask(diffmap, map_dark, config)
+    _, _, prediction_tuple = plot_extrapolation_estimate(
+        diffmap, map_dark, inclusion_mask, config, compact=False
+    )
+
+    parameters = {
+        "folder": config.general.output_folder,
+        "xtr_prefix": "extrapolated"
+        # "d": config.input_files.map_dark,
+    }
+    save_to_folder(diffmap, map_dark, 
+                   parameters,
+                   input_file_config=config.input_files,
+                   save_dict={"best_guess": 1/prediction_tuple[0]},
+                   )
 
 
 
 if __name__ == "__main__":
-    main()
+    extrapolation()
