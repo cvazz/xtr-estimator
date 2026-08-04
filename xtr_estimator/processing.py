ef247d68099b8128a390fe4e754c735c541bd249
Sebastian Bielfeldt <sebastian.bielfeldt@gmx.de>
2026-08-04 13:24:51 +0200

    updated error

--- xtr_estimator/processing.py
+++ xtr_estimator/processing.py
@@ -1051,18 +1051,21 @@ def fill_na_with_model(
 
     return filled_map
 
-def assert_same_high_res_limit(map_dark: rsmap.Map, map_triggered: rsmap.Map, map_dark_comp: rsmap.Map):
+def assert_same_high_res_limit(map_dark: rsmap.Map, map_triggered: rsmap.Map, map_dark_comp: rsmap.Map, map_sampling: int = 3):
     """Ensure that all maps have the same high-resolution limit."""
     dmin_dark = np.min(map_dark.compute_dHKL())
     dmin_triggered = np.min(map_triggered.compute_dHKL())
     dmin_dark_comp = np.min(map_dark_comp.compute_dHKL())
+    shape_dark = map_dark.to_3d_numpy_map(map_sampling=map_sampling)
+    shape_triggered = map_triggered.to_3d_numpy_map(map_sampling=map_sampling)
+    shape_dark_comp = map_dark_comp.to_3d_numpy_map(map_sampling=map_sampling)
 
-    if not (np.isclose(dmin_dark, dmin_triggered) and np.isclose(dmin_dark, dmin_dark_comp)):
+    if not shape_dark == shape_triggered == shape_dark_comp:
         raise ValueError(
             f"High-resolution limits do not match: "
-            f"map_dark: {dmin_dark:.2f}, "
-            f"map_triggered: {dmin_triggered:.2f}, "
-            f"map_dark_comp: {dmin_dark_comp:.2f}. "
+            f"map_dark: {dmin_dark:.2f}, ({shape_dark.shape}) "
+            f"map_triggered: {dmin_triggered:.2f}, ({shape_triggered.shape}) "
+            f"map_dark_comp: {dmin_dark_comp:.2f}, ({shape_dark_comp.shape}) "
             "Please ensure all maps are cut to the same resolution."
         )
 
